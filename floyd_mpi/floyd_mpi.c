5d6f3b65289c20365bce8ceea392a7836ba82261
simplex1337 <cas98@bk.ru>
2017-12-11 12:53:37 +0000

    добавил распледеление матрицы по процессам, нет сборки в одну матрицу

--- floyd_mpi/floyd_mpi.c
+++ floyd_mpi/floyd_mpi.c
@@ -13,24 +13,29 @@ double wtime()
     return (double) t.tv_sec + (double) t.tv_usec * 1E-6;
 }
 
-void raw_distribution(*proc_raws, int size, int raw_num, int k, int *p_raw)
+void min(int a, int b)
 {
-
+    return (a < b) ? a : b;
 }
 
-void floyd_mpi(int *proc_raws, int size, int raw_num)
+void get_chunk(int a, int b, int commsize, int rank, int *lb, int *ub)
 {
-
-}
-
-void data_distribution(int *g, *proc_raws, int size, int raw_num)
-{
-
-}
-
-void result_collection(int *g, int proc_raws, int raw_num)
-{
-
+    int n = b - a + 1;
+    int q = n / commsize;
+    if (n % commsize)
+        q++;
+    int r = commsize * q - n;
+    /* Compute chunk size for the process */
+    int chunk = q;
+    if (rank >= commsize - r) chunk = q - 1;
+    *lb = a; /* Determine start item for the process */
+    if (rank > 0) { /* Count sum of previous chunks */
+        if (rank <= commsize - r)
+            *lb += q * rank;
+        else
+            *lb += q * (commsize - r) + (q - 1) * (rank - (commsize - r));
+    }
+    *ub = *lb + chunk - 1;
 }
 
 static inline void infinitize(int n, int *g)
@@ -81,6 +86,68 @@ void write_matrix(const char *fname, int n, int *a)
     fclose(fp);
 }
 
+// функция для рассылки строки всем процессам
+void raw_distribution(*proc_raws, int n, int raw_num, int k, int *p_raw)
+{
+    int proc_raw_rank; //ранг процесса, которому принадлежит k-я строка
+    int proc_raw_num; //номер k-й строки в полосе матрицы
+
+    //нахождение ранга процесса - владельца k-й строки
+    int rest_raws = n;
+    int ind = 0;
+    int num = n / commsize;
+    for (proc_raw_rank = 1; proc_raw_rank < commsize + 1; proc_raw_rank++) {
+        if (k < ind + num)
+            break;
+        rest_raws -= num;
+        ind += num;
+        num = rest_raws / (commsize - proc_raw_rank);
+    }
+    proc_raw_rank = proc_raw_rank - 1;
+    proc_raw_num = k - ind;
+
+    //копирование строки  в массив raw
+    if (proc_raw_rank == rank)
+        copy(&proc_raws[proc_raw_num * n], &proc_raws[(proc_raw_num + 1) * n], raw)
+    //распределение k-й строки между процессами
+    MPI_Bcast(raw, n, MPI_INT, proc_raw_rank, MPI_COMM_WORLD);
+
+}
+
+void floyd_mpi(int *proc_raws, int n, int raw_num)
+{
+    int *raw = (int *) calloc(n, sizeof(int));
+    int t1, t2;
+
+    for (int k = 0; k < n; k++) {
+        //распределение k-й строки среди процессов
+        raw_distribution(proc_raws, n, raw_num, k, raw)
+        // обновление  элементов матрицы смежности
+        for (int i = 0; i < raw_num; i++) {
+            for (int j = 0; j < n; j++) {
+                if( (proc_raws[i * n + k] != -1) && (raw[j] != -1)) {
+                    t1 = proc_raws[i * n + j];
+                    t2 = proc_raws[i * n + k] + raw[j];
+                    proc_raws[i * n + j] = min(t1, t2);
+                }
+            }
+        }
+    }
+    free(raw);
+}
+
+//распределение данных между процессами
+void data_distribution(int *g, *proc_raws, int n, int raw_num)
+{
+
+}
+
+void result_collection(int *g, int proc_raws, int raw_num)
+{
+
+}
+
+
 int main(int argc, char **argv)
 {
     int n = (argc > 1) ? atoi(argv[1]) : 100;
@@ -91,25 +158,33 @@ int main(int argc, char **argv)
         fprintf(stderr, "Invalid size of graph");
         exit(EXIT_FAILURE);
     }
-    int *g = gen_graph(n);
+    int *g = gen_graph(n); //инициализация
     if (genname)
         write_matrix(genname, n, g);
     int rank, commsize;
-    int *proc_raws; //строки матрицы смежности текущего процесса
-    int raw_num; //чисто строк для текущего процесса
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    int lb, ub;
+    get_chunk(0, m - 1, commsize, rank, &lb, &ub);
+    int raw_num = ub - lb + 1; //чисто строк для текущего процесса
+    //строки матрицы смежности текущего процесса
+    int *proc_raws = malloc(sizeof(*proc_raws) * raw_num * n);
+    for (int i = lb; i < raw_num; i++) {
+        for (int j = 0; j < n; j++)
+            proc_raws[i * n + j] = (g[i * n + j] == 0) ? 10000 : g[i * n + j];
+        }
+    // int *gp = (int *) calloc(n * n, sizeof(int));
+    // memcpy(gp, g, n * n * sizeof(int)); //матрица хранится в каждом процессе
+    // infinitize(n, proc_raws);
 
-    int *gp = (int *) calloc(n * n, sizeof(int));
-    memcpy(gp, g, n * n * sizeof(int));
-    infinitize(n, gp);
+    //my code here(data_distribution(+?), floyd_mpi(+),result_collection)
 
-    //my code here
+    floyd_mpi(proc_raws, n, raw_num);
 
     for (int i = 0; i < n * n; i += n + 1)
         gp[i] = 0;
-    deinfinitize(n, gp);
+    // deinfinitize(n, proc_raws);
 
     // printf("n:     %d\n", n);
     // printf("Time parallel (sec): %.6f\nS(n): %.6f\n", tp);
